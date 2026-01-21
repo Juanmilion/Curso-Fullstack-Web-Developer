474e41cca2742b754bee4b668fedc2ddc4e0ab4b
Juanma <juanmaguerrerx@gmail.com>
2026-01-21 23:07:45 +0100

    Renombrar carpetas

---
+++ modulo-3-typescript/ejercicio1/js/main.js
@@ -0,0 +1,8 @@
+// Definición de variables con tipos explícitos
+var nombre = "Juanma";
+var edad = "veinticinco"; // Esto generará un error de tipo
+var activo = true;
+// Imprimir las variables en la consola
+console.log(nombre);
+console.log(edad);
+console.log(activo);
---
+++ modulo-3-typescript/ejercicio1/ts/main.ts
@@ -0,0 +1,9 @@
+// Definición de variables con tipos explícitos
+let nombre: string = "Juanma";
+let edad: any = "veinticinco"; // Esto generará un error de tipo
+let activo: boolean = true;
+
+// Imprimir las variables en la consola
+console.log(nombre);
+console.log(edad);
+console.log(activo);
